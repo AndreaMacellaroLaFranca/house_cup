e9a0f1e8c1858da5c8037145b23cdb66ddde20a7
Ilies Chikhaoui <ilieschikhaoui@gmail.com>
2024-12-10 23:45:26 +0100

    Implementa form non completata

--- src/app/model/houses/house-service.ts
+++ src/app/model/houses/house-service.ts
@@ -3,13 +3,18 @@ import { HouseDetails } from "./house-details";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { HouseRankingDto } from "./house-ranking-dto";
+import { HttpConfig } from "../../config/http-config";
 
 @Injectable({
     providedIn: 'root'
 })
 export class HouseService {
+    private urlExtension: string = '/house';
     constructor(private http: HttpClient) { }
     getHouseDetails(): Observable<HouseRankingDto> {
         return this.http.get<HouseRankingDto>("http://localhost:8080/ranking");
+    }
+    getHouses(): Observable<HouseDetails[]>{
+        return this.http.get<HouseDetails[]>(`${HttpConfig.apiUrl}${this.urlExtension}`);
     }
 }
